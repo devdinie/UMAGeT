3b29e16d738091d35ce28e66e2a37745faf4319c
devdinie <devdinie@gmail.com>
2022-06-24 23:46:48 -0400

    Reducing input arguments

--- argparser.py
+++ argparser.py
@@ -23,77 +23,24 @@ import argparse
 parser = argparse.ArgumentParser(
     description="Train 3D U-Net model", add_help=True,
     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument("--mode",
-                    default=settings.MODE,
+
+parser.add_argument("--mode", default=settings.mode,
                     help="Train or test")
-parser.add_argument("--network",
-                    default=2,
+
+parser.add_argument("--network", default=2,
                     help="Network 1 or 2")
-parser.add_argument("--root_dir",
-                    default=settings.ROOT_DIR,
-                    help="Root directory for model and all dev code")
-parser.add_argument("--data_path",
-                    default=settings.DATA_PATH,
-                    help="Directory for data: T1 images and labels")
-parser.add_argument("--epochs",
-                    type=int,
-                    default=settings.EPOCHS,
+
+parser.add_argument("--epochs", type=int, default=settings.epochs,
                     help="Number of epochs")
-parser.add_argument("--saved_model1_name",
-                    default=settings.SAVED_MODEL1_NAME,
-                    help="Save model1 (localization) to this path")
-parser.add_argument("--saved_model2_name",
-                    default=settings.SAVED_MODEL2_NAME,
-                    help="Save model2 (segmentation) to this path")
-parser.add_argument("--batch_size",
-                    type=int,
-                    default=settings.BATCH_SIZE,
-                    help="Training batch size")
-parser.add_argument("--tile_height",
-                    type=int,
-                    default=settings.TILE_HEIGHT,
-                    help="Size of the 3D patch height")
-parser.add_argument("--tile_width",
-                    type=int,
-                    default=settings.TILE_WIDTH,
-                    help="Size of the 3D patch width")
-parser.add_argument("--tile_depth",
-                    type=int,
-                    default=settings.TILE_DEPTH,
-                    help="Size of the 3D patch depth")
-parser.add_argument("--number_input_channels",
-                    type=int,
-                    default=settings.NUMBER_INPUT_CHANNELS,
-                    help="Number of input channels")
-parser.add_argument("--number_output_classes",
-                    type=int,
-                    default=settings.NUMBER_OUTPUT_CLASSES,
+
+parser.add_argument("--number_output_classes", type=int,
+                    default=settings.no_output_classes,
                     help="Number of output classes")
-parser.add_argument("--train_test_split",
-                    type=float,
-                    default=settings.TRAIN_TEST_SPLIT,
-                    help="Train/test split (0-1)")
-parser.add_argument("--validate_test_split",
-                    type=float,
-                    default=settings.VALIDATE_TEST_SPLIT,
-                    help="Validation/test split (0-1)")
-parser.add_argument("--print_model",
-                    action="store_true",
-                    default=settings.PRINT_MODEL,
-                    help="Print the summary of the model layers")
-parser.add_argument("--filters",
-                    type=int,
-                    default=settings.FILTERS,
-                    help="Number of filters in the first convolutional layer")
-parser.add_argument("--use_upsampling",
-                    action="store_true",
-                    default=settings.USE_UPSAMPLING,
-                    help="Use upsampling instead of transposed convolution")
+
+parser.add_argument("--train_test_split", type=float,
+                    default=settings.train_test_split,
+                    help="Split between train and test data (value between 0 and 1))")
 parser.add_argument("--augment_inputs",
                     default=settings.AUGMENT,
-                    help="Augment input data: i.e brains and labels")
-parser.add_argument("--random_seed",
-                    default=settings.RANDOM_SEED,
-                    help="Random seed for determinism")
-
+                    help="To input ")
 args = parser.parse_args()
